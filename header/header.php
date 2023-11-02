146fad2c2e4f4d86efb7353898dbc2679a3fd5df
Animesh Kumar <animeshkumarzone@gmail.com>
2023-11-02 17:23:37 +0530

    add header and style

--- header/header.php
+++ header/header.php
@@ -1,9 +1,97 @@
 <!doctype html>
 <html lang="en">
-  <head>
+
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Digital Marketing</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-  </head>
-  <body>
+    <!-- add fontawesome  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
+        integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- add bootstrap  -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <!-- custom style  -->
+    <link rel="stylesheet" href="./assets/css/main.css">
+</head>
+
+<body>
+    <header>
+        <div class="top-header p-3">
+            <div class="container-fluid">
+                <div class="row align-items-center">
+                    <div class="col-md-3 d-flex justify-content-between">
+                        <div class="icon-wrapper">
+                            <div class="icon"><i class="fa-solid fa-file-arrow-up"></i></div>
+                            <div class="icon-title">Upload Your File</div>
+                        </div>
+                        <div class="icon-wrapper">
+                            <div class="icon"><i class="fa-solid fa-image"></i></div>
+                            <div class="icon-title">Request a Sample</div>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="bg-orange">
+                            <div class="icon-wrapper">
+                                <div class="icon"><i class="fa-solid fa-phone"></i></div>
+                                <div class="icon-title"><span class="orange">Printing Solutions</span> <b>Sales:</b>
+                                    (+41)-888-56-7890, <b>Service:</b> 1800.123.4567</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="social-wrapper d-flex justify-content-end" style="gap: 10px;">
+                            Follow On <div class="social-icon"><i class="fa-brands fa-facebook-f"></i></div>
+                            <div class="social-icon"><i class="fa-brands fa-linkedin-in"></i></div>
+                            <div class="social-icon"><i class="fa-brands fa-instagram"></i></div>
+                            <div class="social-icon"><i class="fa-brands fa-twitter"></i></div>
+                            <div class="social-icon"><i class="fa-brands fa-youtube"></i></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#"><img src="./assets/images/logo.png" alt="" srcset=""></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">About</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                                aria-expanded="false">
+                                Services
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="#">Web Development</a></li>
+                                <li><a class="dropdown-item" href="#">App Development</a></li>
+                                <li><a class="dropdown-item" href="#">Digital Marketing</a></li>
+                                <li><a class="dropdown-item" href="#">E-Commerce </a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Contact</a>
+                        </li>
+                    </ul>
+                    <form class="d-flex align-items-center" role="search" >
+                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                        <a style="margin-left: -35px;" type="submit" ><i class="fa-solid fa-magnifying-glass"></i></a>
+                    </form>
+                    <div class="btn-wrapper">
+                      <a class="btn" href="#">GET A QUOTE</a>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </header>
--- index.php
+++ index.php
@@ -1,9 +1,11 @@
-<?php include 'header.php' ?>
-    <h1>Hello, world!</h1>
+<!-- header add  -->
+<?php include 'header/header.php' ?>
+
+
+<!-- start body  -->
+    <h1>Digital Marketing</h1>
 
 
 
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-  </body>
-</html>
+<!-- footer add  -->
+<?php include 'footer/footer.php' ?>
